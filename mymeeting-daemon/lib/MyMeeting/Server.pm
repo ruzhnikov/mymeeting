0041c7b06cbeea4c04a682d951b99e711a95c463
Alexander Ruzhnikov <ruzhnikov85@gmail.com>
2014-04-02 23:20:19 +0400

    * written first test * minor improvements

--- mymeeting-daemon/lib/MyMeeting/Server.pm
+++ mymeeting-daemon/lib/MyMeeting/Server.pm
@@ -3,4 +3,30 @@ package MyMeeting::Server;
 use strict;
 use warnings;
 
+use AnyEvent::Socket;
+
+use constant {
+    DEFAULT_ADDRESS => '127.0.0.1',
+    DEFAULT_PORT    => 60020,
+};
+
+sub new {
+    my ( $class, %params ) = shift;
+
+    $params{server_address} ||= DEFAULT_ADDRESS;
+    $params{server_port}    ||= DEFAULT_PORT;
+
+    my $self = bless { %params }, $class;
+
+    $self->_init;
+
+    return $self;
+}
+
+sub _init {
+    my ( $self ) = @_;
+
+    # ...
+}
+
 1;
